6646befb12904e01ee16ae8a033445b76e8190c4
turrud <fatur14022@gmail.com>
2024-12-05 07:58:13 +0700

    admin

--- app/Providers/Filament/AdminDdnPanelProvider.php
+++ app/Providers/Filament/AdminDdnPanelProvider.php
@@ -38,7 +38,8 @@ class AdminDdnPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
+                
             ])
             ->middleware([
                 EncryptCookies::class,
